ae19e3ab59f3c82b0d02c44faf101ee35a804497
Danniel Condez <ducondez@gmail.com>
2015-04-05 22:46:47 -0700

    create query skeleton and spec file

--- spec/query-spec.js
+++ spec/query-spec.js
@@ -1,27 +1,33 @@
 describe("Query", function() {
+
+  describe("init", function() {
+    it('ensures required options', function() {
+      
+    });
+  });
   
   describe("find", function() {
     it('does something', function() {});
   });
-  
+
   describe("sort", function() {
     it('does something', function() {});
   });
-  
+
   describe("movePage", function() {
     it('does something', function() {});
   });
-  
+
   describe("next", function() {
     it('does something', function() {});
   });
-  
+
   describe("prev", function() {
     it('does something', function() {});
   });
-  
+
   describe("getPages", function() {
     it('does something', function() {});
   });
-  
+
 });
--- src/query.js
+++ src/query.js
@@ -1,19 +1,66 @@
 class Query {
-  
-  constructor() {
+
+  constructor(url, options) {
+
+    // var defaultParams = httpParams ||
+    //   {
+    //     method: 'GET',
+    //     url: url,
+    //     params: {},
+    //     cache: false
+    //   };
+
+    // this.httpParams = extend({}, defaultParams, httpParams);
+    // this.config = extend({}, config);
+
+    // this.data = [];
+
+    // *
+    //  * Filter properties
+     
+    // this.limit = this.config.limit||15;
+    // this.filters = {};
+    // this.lastSearch = false;
+
+    // /**
+    //  * Paging properties
+    //  */
+    // this.page = 1;
+    // this.pages = 1;
+    // this.count = 0;
+
+    // /**
+    //  * Sorting properties
+    //  */
+    // this.sortBy = '';
+    // this.sortAsc = false;
+
+
+    
+  }
+
+  find() {
 
   }
 
-  find() {}
+  sort() {
 
-  sort() {}
+  }
 
-  movePage() {}
+  movePage() {
 
-  next() {}
+  }
 
-  prev() {}
+  next() {
 
-  getPages() {}
+  }
+
+  prev() {
+
+  }
+
+  getPages() {
+
+  }
 
 }
